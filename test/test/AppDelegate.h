49ef33601679feabc04b9de9bb8aac33e0e8ee1d
xupan <xupan@haodf.com>
2016-12-24 19:51:53 +0800

    新建工程

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 徐攀 on 2016/12/24.
+//  Copyright © 2016年 com.borderXLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
