78fe2d58777e2b90ec656b5295633ad920479ef3
xupan <xupan@haodf.com>
2016-12-24 20:11:28 +0800

    mas_make/mas_remake/mas_update的区别和简单用法！

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIButton *redButton;
+
 @end
 
 @implementation ViewController
@@ -17,6 +19,65 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.redButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.redButton addTarget:self action:@selector(redButtonTouchAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.redButton setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:self.redButton];
+    [self.redButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(100);
+        make.left.mas_equalTo(10);
+        make.right.mas_equalTo(-10);
+        make.height.mas_equalTo(50);
+    }];
+    
+}
+
+
+- (void)redButtonTouchAction:(UIButton *)sender
+{
+    sender.selected = !sender.selected;
+    CGFloat buttonHeight = sender.selected ? 100 : 50;
+//    (
+//     "<MASLayoutConstraint:0x6000000baa00 UIButton:0x7fc1a270a360.height == 50>",
+//     "<MASLayoutConstraint:0x6000000bad60 UIButton:0x7fc1a270a360.height == 100>"
+//     )
+//    
+//    Will attempt to recover by breaking constraint
+//    <MASLayoutConstraint:0x6000000bad60 UIButton:0x7fc1a270a360.height == 100>
+    
+//    [self.redButton mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.mas_equalTo(100);
+//        make.left.mas_equalTo(10);
+//        make.right.mas_equalTo(-10);
+//        make.height.mas_equalTo(100);
+//    }];
+    
+    [self.redButton mas_updateConstraints:^(MASConstraintMaker *make) {
+//        make.top.mas_equalTo(100);
+//        make.left.mas_equalTo(10);
+//        make.right.mas_equalTo(-10);
+        make.height.mas_equalTo(buttonHeight);
+    }];
+
+//    [self.redButton mas_remakeConstraints:^(MASConstraintMaker *make) {
+//        make.top.mas_equalTo(100);
+//        make.left.mas_equalTo(10);
+//        make.right.mas_equalTo(-10);
+//        make.height.mas_equalTo(buttonHeight);
+//    }];
+    
+    
+    [UIView animateWithDuration:0.6
+                          delay:0
+         usingSpringWithDamping:0.4
+          initialSpringVelocity:10
+                        options:0
+                     animations:^{
+                         [self.view layoutIfNeeded];
+                     } completion:^(BOOL finished) {
+                         
+                     }];
 }
 
 
