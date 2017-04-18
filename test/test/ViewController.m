795392cad7a83f238bea56a2b2ec186be896672d
Pan <pan@borderxlab.com>
2017-04-18 13:35:38 +0800

    Reset clear

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -7,88 +7,20 @@
 //
 
 #import "ViewController.h"
-#import <SDCycleScrollView.h>
 
 @interface ViewController ()
 
-@property (nonatomic, strong) NSArray *titles;
 
-@property (nonatomic, strong) UIScrollView *scrollView;
-@property (nonatomic, assign) NSInteger idx;
 
 @end
 
 @implementation ViewController
-//21行有个操作
-//我也在21行有操作
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-  
-    NSMutableArray *muta = [NSMutableArray array];
-    
-    dispatch_queue_t seriaQue = dispatch_queue_create("pan.text.que", DISPATCH_QUEUE_SERIAL);
-    NSLog(@"outbegin");
-    
-    self.scrollView = [[UIScrollView alloc] init];
-    self.scrollView.backgroundColor = [UIColor redColor];
-    self.scrollView.contentSize = CGSizeMake(100, 1000);
-    self.scrollView.frame = self.view.bounds;
-    [self.view addSubview:self.scrollView];
-    
-    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-    
-        for (int i = 0; i < 5; i ++) {
-            NSLog(@"begin%d", i);
-            dispatch_barrier_async(seriaQue, ^{
-                
-                [muta removeAllObjects];
-                for (int i = 0; i < 1000; i++) {
-                    NSLog(@"%@--%d", [NSThread currentThread], i);
-                    [muta addObject:@(i)];
-                }
-                
-                
-                for (NSNumber *number in muta) {
-                    NSLog(@"output:%d", [number intValue]);
-                }
-                NSLog(@"end%d", i);
-            });
-        }
-    });
-    
-    NSLog(@"outEnd");
-}
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-    
-    
-    // This is a method didReceiveMemoryWaining
-    
-    UIView *view = [UIView new];
-    
-    
-    UIFont *font = [UIFont systemFontOfSize:12];
-    
-    //thi se test
-    // 这是第三次提交
-    
-    // 这是master上的提交1
-    
-    
-    // rebase1
-    
-    // 这个是tower的提交
-    
-    
-    UIView *view2 = [UIView new];
-    
-    
-    // 这是rebase4上第一次提交
-    
-    // rebase4第二次提交
 }
 
 
